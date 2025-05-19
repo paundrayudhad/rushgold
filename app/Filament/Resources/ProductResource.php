28ce71f90dc4da3837ce64d0a617e1ec71e0471c
Paundra Yudha Dharmawan <paundra461@gmail.com>
2025-05-19 21:15:58 +0700

    add view action to ProductResource table

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -94,6 +94,7 @@ class ProductResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
